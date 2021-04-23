68e369a50685232cb170c5a683a46fa7e15d306b
UlianaKochetkova <54816870+UlianaKochetkova@users.noreply.github.com>
2021-04-23 11:31:27 +0300

    Add files via upload

---
+++ Задание 2/Задача 1/task2/src/task2/Task2.java
@@ -0,0 +1,18 @@
+package task2;
+
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Cat c=new Cat();
+        Dog d=new Dog();
+        Cow w=new Cow();
+        System.out.println("Cat says:");
+        c.voice();
+        System.out.println("Dog says:");
+        d.voice();
+        System.out.println("Cow says:");
+        w.voice();
+    }
+    
+}
